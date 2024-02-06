ca45ff812b7898ae209dfc690148d287bf7b49c4
C. Titus Brown <titus@idyll.org>
2024-02-06 06:50:04 -0800

    MRG: add `mgmanysearch`; support better abundance stuff; improve docs. (#3)
    * remove requirement for abundance support
    
    * support lack of abundance info in metag
    
    * test no abund output
    
    * test neg rows
    
    * refactor out individual search
    
    * make mgmultisearch work
    
    * rename to mgmanysearch; add test
    
    * refactor & cleanup
    
    * catch 0 intersections
    
    * more better docs
    
    * confirm numbers independently

--- src/sourmash_plugin_containment_search.py
+++ src/sourmash_plugin_containment_search.py
@@ -43,7 +43,7 @@ def _get_screen_width():
 
 class Command_ContainmentSearch(CommandLinePlugin):
     command = 'mgsearch'             # 'scripts <command>'
-    description = __doc__       # output with -h
+    description = "Search for a genome in metagenomes"       # output with -h
     usage = usage               # output with no args/bad args as well as -h
     epilog = epilog             # output with -h
     formatter_class = argparse.RawTextHelpFormatter # do not reformat multiline
@@ -56,6 +56,8 @@ class Command_ContainmentSearch(CommandLinePlugin):
                                help='metagenomes to search')
         subparser.add_argument('-o', '--output', default=None,
                                help='output CSV')
+        subparser.add_argument('--require-abundance', action="store_true",
+                               help='require that metagenomes be sketched with abundance')
 
         add_ksize_arg(subparser, default=31)
         add_moltype_args(subparser)
@@ -69,11 +71,85 @@ class Command_ContainmentSearch(CommandLinePlugin):
                         ksize=args.ksize,
                         moltype=moltype,
                         scaled=args.scaled,
-                        output=args.output)
+                        output=args.output,
+                        require_abundance=args.require_abundance)
+
+
+#
+# CLI plugin - supports 'sourmash scripts mgmanysearch'
+#
+
+class Command_ContainmentManySearch(CommandLinePlugin):
+    command = 'mgmanysearch'             # 'scripts <command>'
+    description = "Search for genomes in metagenomes"       # output with -h
+    usage = usage               # output with no args/bad args as well as -h
+    epilog = epilog             # output with -h
+    formatter_class = argparse.RawTextHelpFormatter # do not reformat multiline
+
+    def __init__(self, subparser):
+        super().__init__(subparser)
+        subparser.add_argument('--queries', '--query', nargs='+',
+                               help='sketches to look for')
+        subparser.add_argument('--against', '--db', '--metagenomes', nargs='+',
+                               help='metagenomes to search')
+        subparser.add_argument('-o', '--output', default=None,
+                               help='output CSV')
+        subparser.add_argument('--require-abundance', action="store_true",
+                               help='require that metagenomes be sketched with abundance')
+
+        add_ksize_arg(subparser, default=31)
+        add_moltype_args(subparser)
+        add_scaled_arg(subparser, default=1000)
+
+    def main(self, args):
+        super().main(args)
+
+        moltype = sourmash_args.calculate_moltype(args)
+        return mg_many_search(args.queries, args.against,
+                              ksize=args.ksize,
+                              moltype=moltype,
+                              scaled=args.scaled,
+                              output=args.output,
+                              require_abundance=args.require_abundance)
+
+
+## Implementation!
+
+COLUMNS = ['intersect_bp',
+           'match_filename',
+           'match_name',
+           'match_md5',
+           'query_filename',
+           'query_name',
+           'query_md5',
+           'ksize',
+           'moltype',
+           'scaled',
+           'f_query',
+           'f_match',
+           'f_match_weighted',
+           'sum_weighted_found',
+           'average_abund',
+           'median_abund',
+           'std_abund',
+           'query_n_hashes',
+           'match_n_hashes',
+           'match_n_weighted_hashes',
+           'jaccard',
+           'genome_containment_ani',
+           'match_containment_ani',
+           'average_containment_ani',
+           'max_containment_ani',
+           'potential_false_negative'
+           ]
 
 
 def mgsearch(query_filename, against_list, *,
-             ksize=31, moltype='DNA', scaled=1000, output=None):
+             ksize=31, moltype='DNA', scaled=1000, output=None,
+             require_abundance=False):
+    """
+    Search for a single genome in many metagenomes.
+    """
     screen_width = _get_screen_width()
 
     query_ss = sourmash.load_file_as_index(query_filename)
@@ -92,37 +168,9 @@ def mgsearch(query_filename, against_list, *,
         query_ss = query_ss.to_mutable()
         query_ss.minhash = query_mh
 
-    columns = ['intersect_bp',
-               'match_filename',
-               'match_name',
-               'match_md5',
-               'query_filename',
-               'query_name',
-               'query_md5',
-               'ksize',
-               'moltype',
-               'scaled',
-               'f_query',
-               'f_match',
-               'f_match_weighted',
-               'sum_weighted_found',
-               'average_abund',
-               'median_abund',
-               'std_abund',
-               'query_n_hashes',
-               'match_n_hashes',
-               'match_n_weighted_hashes',
-               'jaccard',
-               'genome_containment_ani',
-               'match_containment_ani',
-               'average_containment_ani',
-               'max_containment_ani',
-               'potential_false_negative'
-               ]
-
     if output:
         out_fp = open(output, 'w', newline='')
-        out_w = csv.DictWriter(out_fp, fieldnames=columns)
+        out_w = csv.DictWriter(out_fp, fieldnames=COLUMNS)
         out_w.writeheader()
     else:
         out_fp = None
@@ -132,97 +180,246 @@ def mgsearch(query_filename, against_list, *,
 
     # display stuff
     first = True
+
+    # missing abundances?
+    missed_abundance = False
     
     for metag_filename in against_list:
-        metag = sourmash.load_file_as_signatures(metag_filename,
-                                                 ksize=ksize)
-        metag = list(metag)
-        assert len(metag) == 1
-        metag = metag[0]
+        results_d = _search_metag(query_ss, metag_filename, ksize,
+                                  require_abundance,
+                                  screen_width=screen_width,
+                                  field_width=41)
 
-        # make sure metag has abundance!
-        assert metag.minhash.track_abundance, f"'{metag_filename}' must have abundance information"
+        name = results_d['display_name']
+        del results_d['display_name']
 
-        result = PrefetchResult(query_ss, metag, threshold_bp=0,
-                                 estimate_ani_ci=False)
+        has_abundance = results_d['average_abund'] != ''
 
-        # calculate total weighted hashes for use in denominator:
-        total_sum_abunds = metag.minhash.sum_abundances
-
-        # get a flattened copy for use in intersections...
-        flat_metag = metag.minhash.flatten()
-
-        # other info!
-
-        results_template = dict(match_md5=metag.md5sum(),
-                                match_name=metag.name,
-                                match_filename=metag_filename,
-                                ksize=ksize,
-                                moltype=metag.minhash.moltype,
-                                scaled=metag.minhash.scaled)
-
-        # now, get weighted containment for query genome
-        intersect_mh = query_mh.intersection(flat_metag)
-        w_intersect_mh = intersect_mh.inflate(metag.minhash)
-
-        abunds = list(w_intersect_mh.hashes.values())
-
-        if abunds:
-            mean = np.mean(abunds)
-            median = np.median(abunds)
-            std = np.std(abunds)
-            overlap_sum_abunds = w_intersect_mh.sum_abundances
-            f_sum_abunds = overlap_sum_abunds / total_sum_abunds
-        else:
-            mean = median = std = 0
-            overlap_sum_abunds = 0
-            f_sum_abunds = 0
-
-        results_d = dict(intersect_bp=result.intersect_bp,
-                         query_filename=query_ss.filename,
-                         query_name=query_ss.name,
-                         query_md5=query_ss.md5sum(),
-                         f_query=result.f_match_query,
-                         f_match=result.f_query_match,
-                         f_match_weighted=f_sum_abunds,
-                         sum_weighted_found=overlap_sum_abunds,
-                         query_n_hashes=len(query_mh),
-                         match_n_hashes=len(flat_metag),
-                         match_n_weighted_hashes=total_sum_abunds,
-                         average_abund=mean,
-                         median_abund=median,
-                         std_abund=std,
-                         jaccard=result.jaccard,
-                         genome_containment_ani=result.query_containment_ani,
-                         match_containment_ani=result.match_containment_ani,
-                         average_containment_ani=result.average_containment_ani,
-                         max_containment_ani=result.max_containment_ani,
-                         potential_false_negative=result.potential_false_negative,
-                         )
-
-        results_d.update(results_template)
-
+        # write out CSV
         if out_w:
             out_w.writerow(results_d)
 
+        # displaying first result?
         if first:
             print("")
             print("p_genome avg_abund   p_metag   metagenome name")
             print("-------- ---------   -------   ---------------")
             first = False
 
-        name = metag._display_name(screen_width - 41)
-
         f_genome_found = results_d['f_query']
         pct_genome = f"{f_genome_found*100:.1f}"
 
-        f_metag_weighted = results_d['f_match_weighted']
-        pct_metag = f"{f_metag_weighted*100:.1f}"
+        if has_abundance:
+            f_metag_weighted = results_d['f_match_weighted']
+            pct_metag = f"{f_metag_weighted*100:.1f}%"
 
-        avg_abund = results_d['average_abund']
-        avg_abund = f"{avg_abund:.1f}"
+            avg_abund = results_d['average_abund']
+            avg_abund = f"{avg_abund:.1f}"
+        else:
+            avg_abund = "N/A"
+            pct_metag = "N/A"
 
-        print(f'{pct_genome:>6}%  {avg_abund:>6}     {pct_metag:>7}%   {name}')
+        print(f'{pct_genome:>6}%  {avg_abund:>6}     {pct_metag:>6}     {name}')
 
+    # close CSV file
     if out_fp:
         out_fp.close()
+
+    # notify user that there were columns that were not filled in
+    if missed_abundance:
+        notify("")
+        notify("** Note: N/A in column values indicate metagenomes w/o abundance tracking.")
+
+
+def mg_many_search(query_filenames, against_list, *,
+                   ksize=31, moltype='DNA', scaled=1000, output=None,
+                   require_abundance=False):
+    """
+    Search multiple genomes in many metagenomes, loading each metagenome
+    once.
+    """
+    screen_width = _get_screen_width()
+
+    query_sigs = []
+    for query_filename in query_filenames:
+        query_ss = sourmash.load_file_as_index(query_filename)
+        query_ss = query_ss.select(ksize=ksize, moltype=moltype)
+        query_sigs.extend(query_ss.signatures())
+
+    print(f"Loaded {len(query_sigs)} query signatures.")
+
+    # downsample each query sig if necessary.
+    for query_ss in query_sigs:
+        query_mh = query_ss.minhash
+        if scaled is not None and scaled != query_mh.scaled:
+            query_mh = query_mh.downsample(scaled=scaled)
+            query_ss = query_ss.to_mutable()
+            query_ss.minhash = query_mh
+
+    # prepare output
+    if output:
+        out_fp = open(output, 'w', newline='')
+        out_w = csv.DictWriter(out_fp, fieldnames=COLUMNS)
+        out_w.writeheader()
+    else:
+        out_fp = None
+        out_w = None
+
+    # display stuff
+    first = True
+
+    # came across missing abundances?
+    missed_abundance = False
+
+    ### go through metagenomes one by one
+    for metag_filename in against_list:
+        # for each metagenome, iterate over query sigs
+        for query_ss in query_sigs:
+            results_d = _search_metag(query_ss, metag_filename, ksize,
+                                      require_abundance,
+                                      screen_width=screen_width,
+                                      field_width=21)
+
+            name = results_d['display_name']
+            del results_d['display_name']
+
+            has_abundance = results_d['average_abund'] != ''
+
+            # write out CSV
+            if out_w:
+                out_w.writerow(results_d)
+
+            #
+            # display!
+            #
+
+            # displaying first result?
+            if first:
+                print("")
+                print("query             p_genome avg_abund   p_metag   metagenome name")
+                print("--------          -------- ---------   -------   ---------------")
+                first = False
+
+            f_genome_found = results_d['f_query']
+            pct_genome = f"{f_genome_found*100:.1f}"
+
+            if has_abundance:
+                f_metag_weighted = results_d['f_match_weighted']
+                pct_metag = f"{f_metag_weighted*100:.1f}%"
+
+                avg_abund = results_d['average_abund']
+                avg_abund = f"{avg_abund:.1f}"
+            else:
+                avg_abund = "N/A"
+                pct_metag = "N/A"
+
+            query_name = query_ss._display_name(17)
+            print(f'{query_name:<17} {pct_genome:>6}%  {avg_abund:>6}     {pct_metag:>6}     {name}')
+
+            # end each query genome
+        # end each subject metagenome
+
+    # close CSV file
+    if out_fp:
+        out_fp.close()
+
+    # notify user that there were columns that were not filled in
+    if missed_abundance:
+        notify("")
+        notify("** Note: N/A in column values indicate metagenomes w/o abundance tracking.")
+
+
+def _search_metag(query_ss, metag_filename, ksize, require_abundance, *,
+                 screen_width=80, field_width=41):
+    """
+    Do the actual search &c for query in a metagenome.
+    """
+    query_mh = query_ss.minhash
+
+    metag = sourmash.load_file_as_signatures(metag_filename,
+                                             ksize=ksize)
+    metag = list(metag)
+    assert len(metag) == 1
+    metag = metag[0]
+
+    # check to make sure if metag needs & has abundance info
+    if require_abundance:
+        if not metag.minhash.track_abundance:
+            raise ValueError(f"'{metag_filename}' must have abundance information")
+
+    has_abundance = False
+    if metag.minhash.track_abundance:
+        has_abundance = True
+    else:
+        missed_abundance = True
+
+    # calculate stuff!
+    result = PrefetchResult(query_ss, metag, threshold_bp=0,
+                             estimate_ani_ci=False)
+
+    # calculate total weighted hashes for use in denominator:
+    total_sum_abunds = metag.minhash.sum_abundances
+
+    # get a flattened copy for use in intersections...
+    flat_metag = metag.minhash.flatten()
+
+    # other info!
+    results_template = dict(match_md5=metag.md5sum(),
+                            match_name=metag.name,
+                            match_filename=metag_filename,
+                            ksize=ksize,
+                            moltype=metag.minhash.moltype,
+                            scaled=metag.minhash.scaled)
+
+    # this is where we depart from PrefetchResult :)
+    if has_abundance:
+        # now, get weighted containment for query genome
+        intersect_mh = query_mh.intersection(flat_metag) # CTB: redundant?
+
+        if len(intersect_mh):
+            w_intersect_mh = intersect_mh.inflate(metag.minhash)
+
+            abunds = list(w_intersect_mh.hashes.values())
+            mean = np.mean(abunds)
+            median = np.median(abunds)
+            std = np.std(abunds)
+            overlap_sum_abunds = w_intersect_mh.sum_abundances
+            f_sum_abunds = overlap_sum_abunds / total_sum_abunds
+        else:
+            mean = 0
+            median = 0
+            std = 0
+            overlap_sum_abunds = 0
+            f_sum_abunds = 0.0
+    else:
+        mean = median = std = ""
+        overlap_sum_abunds = ""
+        f_sum_abunds = ""
+
+    # calculate final results
+    results_d = dict(intersect_bp=result.intersect_bp,
+                     query_filename=query_ss.filename,
+                     query_name=query_ss.name,
+                     query_md5=query_ss.md5sum(),
+                     f_query=result.f_match_query,
+                     f_match=result.f_query_match,
+                     f_match_weighted=f_sum_abunds,
+                     sum_weighted_found=overlap_sum_abunds,
+                     query_n_hashes=len(query_mh),
+                     match_n_hashes=len(flat_metag),
+                     match_n_weighted_hashes=total_sum_abunds,
+                     average_abund=mean,
+                     median_abund=median,
+                     std_abund=std,
+                     jaccard=result.jaccard,
+                     genome_containment_ani=result.query_containment_ani,
+                     match_containment_ani=result.match_containment_ani,
+                     average_containment_ani=result.average_containment_ani,
+                     max_containment_ani=result.max_containment_ani,
+                     potential_false_negative=result.potential_false_negative,
+                     display_name=metag._display_name(screen_width - field_width)
+                     )
+
+    results_d.update(results_template)
+
+    return results_d
--- tests/test_sourmash_plugin.py
+++ tests/test_sourmash_plugin.py
@@ -38,7 +38,7 @@ def test_0_x_podar(runtmp):
     runtmp.sourmash('scripts', 'mgsearch', query, against)
     
     out = runtmp.last_result.out
-    assert " 100.0%    54.2         3.1%   SRR606249" in out
+    assert "100.0%    54.2       3.1%     SRR606249" in out
 
 
 def test_0_x_podar_out(runtmp):
@@ -84,6 +84,7 @@ def test_0_x_podar_out(runtmp):
 
     row_keys = set(row.keys())
     col_keys = set(columns)
+    assert row_keys == col_keys
 
     assert round(float(row['f_match']), 3) == 0.010
     assert round(float(row['f_match_weighted']), 3) == 0.031
@@ -95,12 +96,9 @@ def test_0_x_podar_out(runtmp):
     assert int(row['sum_weighted_found']) == 2276
     assert int(row['match_n_weighted_hashes']) == 73489
     assert int(row['intersect_bp']) == 4200000
-    assert int(row['match_n_hashes']) == 4200 # @CTB?
+    assert int(row['match_n_hashes']) == 4200
     assert int(row['query_n_hashes']) == 42
 
-    assert row_keys == col_keys
-    #assert 0
-    
 
 def test_1_x_podar(runtmp):
     query = utils.get_test_data('1.sig.zip')
@@ -109,4 +107,55 @@ def test_1_x_podar(runtmp):
     
     out = runtmp.last_result.out
     print(out)
-    assert " 100.0%    45.5         0.4%   SRR606249" in out
+    assert "100.0%    45.5       0.4%     SRR606249" in out
+
+
+def test_1_x_0_require_abundance(runtmp):
+    # require abundance!
+    query = utils.get_test_data('1.sig.zip')
+    against = utils.get_test_data('0.sig.zip')
+
+    with pytest.raises(SourmashCommandFailed):
+        runtmp.sourmash('scripts', 'mgsearch', query, against,
+                        '--require-abundance', fail_ok=True)
+    
+    err = runtmp.last_result.err
+    print(err)
+    assert "must have abundance information" in err
+
+
+def test_1_x_0_no_abund(runtmp):
+    # do not require abundance
+    query = utils.get_test_data('1.sig.zip')
+    against = utils.get_test_data('0.sig.zip')
+
+    runtmp.sourmash('scripts', 'mgsearch', query, against, '-o', 'out.csv')
+    csvfp = open(runtmp.output('out.csv'), newline='')
+    rows = list(csv.DictReader(csvfp))
+    row = rows[0]
+    pprint(row)
+
+    assert round(float(row['f_match']), 3) == 0.0
+    assert int(row['intersect_bp']) == 0
+    assert int(row['match_n_hashes']) == 42
+    assert int(row['query_n_hashes']) == 6
+
+    assert not row['f_match_weighted']
+    assert not row['median_abund']
+    assert not row['average_abund']
+    assert not row['std_abund']
+    assert not row['sum_weighted_found']
+    assert not row['match_n_weighted_hashes']
+
+
+def test_manysearch_0_x_podar(runtmp):
+    query1 = utils.get_test_data('0.sig.zip')
+    query2 = utils.get_test_data('1.sig.zip')
+    against = utils.get_test_data('SRR606249.k31.sig.zip')
+    runtmp.sourmash('scripts', 'mgmanysearch', '--queries', query1, query2,
+                    '--against', against)
+    
+    out = runtmp.last_result.out
+    print(out)
+    assert "CP001472.1 Aci...  100.0%    54.2       3.1%     SRR606249" in out
+    assert "CP001941.1 Aci...  100.0%    45.5       0.4%     SRR606249" in out
